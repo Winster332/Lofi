746bc269487b688870d1dba2c19284f6d4a63ed9
Winster332 <roof.is.on.fire.science@gmail.com>
2019-02-04 12:21:38 +0300

    refactoring

--- Modules/Yandex/Yandex.Music/LofiYandexMusicApi.cs
+++ Modules/Yandex/Yandex.Music/LofiYandexMusicApi.cs
@@ -28,6 +28,7 @@ namespace Yandex.Music
     public static string Title => Assembly.GetEntryAssembly().GetTitle();
     public static string Description => Assembly.GetEntryAssembly().GetDescription();
     public static string Version => Assembly.GetEntryAssembly().GetVersion();
+    public YandexMusicSettings _settings { get; set; }
 
     private string _login;
     private string _password;
@@ -37,6 +38,8 @@ namespace Yandex.Music
 
     public LofiYandexMusicApi()
     {
+      _settings = new YandexMusicSettings();
+      
       NetworkChange.NetworkAvailabilityChanged += (sender, args) =>
       {
         Log.Information($"Networking: {args.IsAvailable}");
@@ -56,7 +59,7 @@ namespace Yandex.Music
       _password = password;
       
       var result = false;
-      var _passportUri = new Uri("https://pda-passport.yandex.ru/passport?mode=auth");
+      var _passportUri = _settings.GetPassportURL();
       
       var request = GetRequest(_passportUri,
         new KeyValuePair<string, string>("login", _login),
@@ -87,7 +90,7 @@ namespace Yandex.Music
 
     public Album GetAlbum(string albumId)
     {
-      var request = GetRequest(new Uri($"https://music.yandex.ru/handlers/album.jsx?album={albumId}&lang=ru&external-domain=music.yandex.ru&overembed=false&ncrnd=0.7993721501733155"),  WebRequestMethods.Http.Get);
+      var request = GetRequest(_settings.GetAlbumURL(albumId),  WebRequestMethods.Http.Get);
       var album = default(Album);
       
       using (var response = (HttpWebResponse) request.GetResponse())
@@ -106,7 +109,7 @@ namespace Yandex.Music
       if (login == null)
         login = _login;
       
-      var request = GetRequest(new Uri($"https://music.yandex.ru/handlers/library.jsx?owner={login}&filter=tracks&likeFilter=favorite&sort=&dir=&lang=ru&external-domain=music.yandex.ru&overembed=false&ncrnd=0.7506943983987266"));
+      var request = GetRequest(_settings.GetListFavoritesURL(login));
       var tracks = new List<Track>();
       
       using (var response = (HttpWebResponse) request.GetResponse())
@@ -124,7 +127,7 @@ namespace Yandex.Music
 
     public Playlist GetPlaylistDejaVu()
     {
-      var request = GetRequest(new Uri($"https://music.yandex.ru/handlers/playlist.jsx?owner=yamusic-dejavu&kinds=57704235&light=true&madeFor=&lang=ru&external-domain=music.yandex.ru&overembed=false&ncrnd=0.13048851242872916"));
+      var request = GetRequest(_settings.GetPlaylistDejaVuURL());
       var playlist = default(Playlist);
       
       using (var response = (HttpWebResponse) request.GetResponse())
@@ -141,7 +144,7 @@ namespace Yandex.Music
     
     public Playlist GetPlaylistOfDay()
     {
-      var request = GetRequest(new Uri($"https://music.yandex.ru/handlers/playlist.jsx?owner=yamusic-daily&kinds=57151881&light=true&madeFor=&lang=ru&external-domain=music.yandex.ru&overembed=false&ncrnd=0.9083773647705418"));
+      var request = GetRequest(_settings.GetPlaylistOfDay());
       var playlist = default(Playlist);
       
       using (var response = (HttpWebResponse) request.GetResponse())
@@ -160,7 +163,8 @@ namespace Yandex.Music
     {
       try
       {
-        var trackDownloadUrl = GetURLDownloadTrack(track);
+        var trackDonloadInfo = GetDownloadTrackInfo(track.StorageDir);
+        var trackDownloadUrl = _settings.GetURLDownloadTrack(track, trackDonloadInfo);
         var isNetworing = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
 
         using (var client = new WebClient())
@@ -180,7 +184,8 @@ namespace Yandex.Music
 
     public StreamTrack ExtractStreamTrack(Track track)
     {
-      var trackDownloadUrl = GetURLDownloadTrack(track);
+      var trackDonloadInfo = GetDownloadTrackInfo(track.StorageDir);
+      var trackDownloadUrl = _settings.GetURLDownloadTrack(track, trackDonloadInfo);
 
       var isNetworing = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
 
@@ -189,7 +194,8 @@ namespace Yandex.Music
 
     public byte[] ExtractDataTrack(Track track)
     {
-      var trackDownloadUrl = GetURLDownloadTrack(track);
+      var trackDonloadInfo = GetDownloadTrackInfo(track.StorageDir);
+      var trackDownloadUrl = _settings.GetURLDownloadTrack(track, trackDonloadInfo);
       byte[] bytes;
       
       using (var client = new WebClient())
@@ -238,14 +244,8 @@ namespace Yandex.Music
     private List<ISearchable> Search(string searchText, SearchType searchType, int page = 0)
     {
       var listResult = new List<ISearchable>();
-      var searchTypeAsString = searchType.ToString();
-      var urlSearch = new StringBuilder();
-      urlSearch.Append($"https://music.yandex.ru/handlers/music-search.jsx?text={searchText}");
-      urlSearch.Append($"&type={searchTypeAsString}");
-      urlSearch.Append(
-        $"&page={page}&ncrnd=0.7060701951464323&lang=ru&external-domain=music.yandex.ru&overembed=false");
 
-      var request = GetRequest(new Uri(urlSearch.ToString()), WebRequestMethods.Http.Get);
+      var request = GetRequest(_settings.GetSearchURL(searchText, searchType, page), WebRequestMethods.Http.Get);
 
       using (var response = (HttpWebResponse) request.GetResponse())
       {
@@ -278,44 +278,10 @@ namespace Yandex.Music
       return listResult;
     }
 
-    protected Uri GetURLDownloadTrack(Track track)
-    {
-      var downloadInfo = GetDownloadTrackInfo(track.StorageDir);
-      var key = "";//downloadInfo.Path.Substring(1, downloadInfo.Path.Length - 1) + downloadInfo.S;
-
-      using (var md5 = MD5.Create())
-      {
-        key = GetMdHesh(md5, $"XGRlBW9FXlekgbPrRHuSiA{downloadInfo.Path.Substring(1, downloadInfo.Path.Length - 1)}{downloadInfo.S}");
-      }
-
-      var trackDownloadUrl =
-        String.Format("http://{0}/get-mp3/{1}/{2}{3}?track-id={4}&region=225&from=service-search",
-          downloadInfo.Host, 
-          key, 
-          downloadInfo.Ts,
-          downloadInfo.Path,
-          track.Id);
-
-      return new Uri(trackDownloadUrl);
-    }
-
-    protected string GetMdHesh(MD5 md5, string str)
-    {
-      var data = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
-      var sBuilder = new StringBuilder();
-
-      for (var i = 0; i < data.Length; i++)
-      {
-        sBuilder.Append(data[i].ToString("x2"));
-      }
-
-      return sBuilder.ToString();
-    }
-
     protected TrackDownloadInfo GetDownloadTrackInfo(string storageDir)
     {
       var fileName = GetDownloadTrackInfoFileName(storageDir);
-      var request = GetRequest(new Uri($"http://storage.music.yandex.ru/download-info/{storageDir}/{fileName}"));
+      var request = GetRequest(_settings.GetDownloadTrackInfoURL(storageDir, fileName));
       var trackDownloadInfo = new TrackDownloadInfo();
 
       using (var response = (HttpWebResponse) request.GetResponse())
@@ -346,7 +312,7 @@ namespace Yandex.Music
 
     protected string GetDownloadTrackInfoFileName(string storageDir)
     {
-      var request = GetRequest(new Uri($"http://storage.music.yandex.ru/get/{storageDir}/2.xml"), WebRequestMethods.Http.Get);
+      var request = GetRequest(_settings.GetURLDownloadFile(storageDir), WebRequestMethods.Http.Get);
       var fileName = "";
       var trackLength = 0;
       
