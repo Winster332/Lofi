e42c94fdc1885c3ff8d00fb04d1633d37baff966
Winster332 <stas-martynov-98@mail.ru>
2019-02-07 21:05:46 +0300

    cleanup

--- Modules/Lofy.Modules.Core/Autorize/IAuthorize.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Lofy.Modules.Core.Autorize
-{
-  public interface IAuthorize
-  {
-    bool Login(string username, string password);
-  }
-}
--- Modules/VK/VK.Music/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VK.Music
-{
-  public class Class1
-  {
-  }
-}
